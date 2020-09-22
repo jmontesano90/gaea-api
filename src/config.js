bf3c4e2129dc5963b95134fcf875cad603cd58ee
Joseph Montesano <jmmmontesano@gmail.com>
2020-09-22 19:47:02 -0400

    added live server

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/gaea',
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://postgres@localhost/gaea',
   JWT_SECRET:
     process.env.JWT_SECRET ||
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwb28iLCJuYW1lIjoiSm9lIENvb2wiLCJpYXQiOjE1MTYyMzkwMjJ9.4tRh3eVlYVN5rc1b4LwujSTBhtb3Dkx4Hkbk5iE79vY',
